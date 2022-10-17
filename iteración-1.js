86091c8a57a0a8f387b29adbafbd0b53cea42a80
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-17 19:29:19 +0200

    DONE, exircese 1

--- iteración-1.js
+++ iteración-1.js
@@ -2,6 +2,18 @@
 // que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre 
 // por consola la suma de los dos parametros.
 
+const sum = (a = 10, b = 5) => {
+    return console.log(a + b);
+};
+
 // 1.1 Ejecuta esta función sin pasar ningún parametro
+
+sum();
+
 // 1.2 Ejecuta esta función pasando un solo parametro
+
+sum(1);
+
 // 1.3 Ejecuta esta función pasando dos parametros
+
+sum(2, 3);
